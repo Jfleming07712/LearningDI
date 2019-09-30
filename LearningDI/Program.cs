f3da52cf87fb5a7428cc064e6b3a1612b09a8bf6
Jfleming <Jfleming07712@gmail.com>
2019-09-30 17:28:05 -0400

    Add project files.

---
+++ LearningDI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearningDI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
